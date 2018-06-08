558c957121deb910b3a9ba2b21ba07aa1d669fd4
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-08 14:10:11 +0530

    port randomisation

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
+const PORT = process.env.PORT || 5000
 
-
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 app.get('/', function(req, res){
@@ -10,6 +17,4 @@ app.get('/', function(req, res){
 
 });
 
-http.listen(1337, function(){
-  console.log('listening on *:1337');
-});
+
